10052e8dc0e6294a1bec54d6d6ea6ddc5b9f9cc5
Thomás Dixini <thomasdixini@gmail.com>
2024-10-25 07:25:51 -0300

    Começando a implementar camada do tabuleiro

---
+++ ProjetoXadrez/Tabuleiro/Peca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoXadrez.Tabuleiro
+{
+    internal class Peca
+    {
+        public Cor cor;
+        public Posicao posicao;
+        public Tabuleiro tabuleiro;
+        public int qtdMovimentos;
+
+        public Peca(Cor cor, Posicao posicao, Tabuleiro tabuleiro)
+        {
+            this.cor = cor;
+            this.posicao = posicao;
+            this.tabuleiro = tabuleiro;
+            qtdMovimentos = 0;
+        }
+    }
+}
---
+++ ProjetoXadrez/Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoXadrez.Tabuleiro
+{
+    internal class Tabuleiro
+    {
+        public int linhas;
+        public int colunas;
+        public Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            this.pecas = new Peca[linhas, colunas];
+        }
+    }
+}
