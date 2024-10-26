e5904e01de3bdb846b42964a5a306bb33eb6ff3c
Thomás Dixini <thomasdixini@gmail.com>
2024-10-26 14:22:14 -0300

    Classe que imprime tabuleiro na tela

---
+++ ProjetoXadrez/Tela.cs
@@ -0,0 +1,23 @@
+﻿using ProjetoXadrez.tabuleiro;
+
+namespace ProjetoXadrez
+{
+    internal class Tela
+    {
+        public static void ImprimeTabuleiro(Tabuleiro tab)
+        {
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (tab.pecas[i, j] == null)
+                    {
+                        Console.Write("-" + " ");
+                    }
+                    Console.Write(tab.pecas[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
