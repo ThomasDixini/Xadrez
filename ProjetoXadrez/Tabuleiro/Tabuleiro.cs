2114fda290a177fe4738a9f18f8ecebcd3a61bfb
Thomás Dixini <thomasdixini@gmail.com>
2024-10-28 07:31:35 -0300

    Criando validacoes da classe tabuleiro

--- ProjetoXadrez/Tabuleiro/Tabuleiro.cs
+++ ProjetoXadrez/Tabuleiro/Tabuleiro.cs
@@ -19,10 +19,43 @@ namespace ProjetoXadrez.tabuleiro
             this.pecas = new Peca[linhas, colunas];
         }
 
+        public Peca peca(Posicao posicao)
+        {
+            return pecas[posicao.linha, posicao.coluna];
+        }
+
         public void colocarPeca(Peca peca, Posicao posicao)
         {
+            if (existePeca(posicao))
+            {
+                throw new TabuleiroException("Peca ja existe na posicao!");
+            }
             pecas[posicao.linha, posicao.coluna] = peca;
             peca.posicao = posicao;
+        }
+
+        public bool existePeca(Posicao posicao)
+        {
+            validaPosicao(posicao);
+            return peca(posicao) != null;
+        }
+
+        public bool posicaoValida(Posicao posicao)
+        {
+            if (posicao.linha < 0 || posicao.linha >= linhas || posicao.coluna < 0 || posicao.coluna >= colunas)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void validaPosicao(Posicao posicao)
+        {
+            if (!posicaoValida(posicao))
+            {
+                throw new TabuleiroException("Posicao Invalida!");
+            }
         }
     }
 }
